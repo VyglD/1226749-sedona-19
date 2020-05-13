89f4eaf84ef7c2992cf361d1ba339ebad09dd763
VyglD <VyglD@yandex.ru>
2020-05-14 00:07:32 +0300

    adaptive graphic

--- source/js/logic.js
+++ source/js/logic.js
@@ -1,31 +1,39 @@
-var closeBtn = document.querySelector('#close-btn-menu');
-var toggleBtn = document.querySelector('#toggle-btn-menu');
-var nav = document.querySelector('#nav');
+var closeBtn = document.querySelector('#close-btn-menu-js');
+var toggleBtn = document.querySelector('#toggle-btn-menu-js');
+var nav = document.querySelector('#nav-js');
 
 function closeNav() {
-    nav.classList.remove('main-nav__list--open');
-    nav.classList.add('main-nav__list--close');
-    nav.classList.add('main-nav__list--animate');
+    nav.classList.remove('main-nav__list--open-js');
+    nav.classList.add('main-nav__list--close-js');
+    nav.classList.add('main-nav__list--animate-js');
+
+    toggleBtn.classList.remove('main-nav__toggle-button--open-js');
 }
 
-function removeAnimate() {
-    nav.classList.remove('main-nav__list--animate');
+function openNav() {
+    nav.classList.remove('main-nav__list--close-js');
+    nav.classList.add('main-nav__list--open-js');
+    nav.classList.add('main-nav__list--animate-js');
+
+    toggleBtn.classList.add('main-nav__toggle-button--open-js');
 }
 
-nav.classList.add('main-nav__list--close');
-
-document.body.onresize = removeAnimate;
+window.addEventListener('resize', function() {
+    nav.classList.remove('main-nav__list--animate-js');
+});
 
 closeBtn.addEventListener('click', function() {
     closeNav();
 });
 
 toggleBtn.addEventListener('click', function() {
-    if(nav.classList.contains('main-nav__list--close')) {
-        nav.classList.remove('main-nav__list--close');
-        nav.classList.add('main-nav__list--open');
-        nav.classList.add('main-nav__list--animate');
+    if(nav.classList.contains('main-nav__list--close-js')) {
+        openNav();
     } else {
         closeNav();
     }
 });
+
+closeBtn.classList.remove('main-nav__close-button--nojs');
+toggleBtn.classList.remove('main-nav__toggle-button--nojs');
+nav.classList.add('main-nav__list--close-js');
